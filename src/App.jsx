d0ba224b12fd8def5f2e2f8711b7bcf7ed2ce942
Adrianna <adriannamoffitt06@gmail.com>
2023-11-28 17:15:25 -0600

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, Link } from "react-router-dom"
 import './App.css'
-
+import './Components/Red'
+import './Components/Blue'
+import Homepage from "./Components/Home"
+import Blue from "./Components/Blue"
+import Red from "./Components/Red"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div id="container">
-        <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
-    </div>
-    </>
+    <div id="container">
+      <div id="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/blue">Blue</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
+        </Routes>
+        </div>
+  </div>
   )
 }
 
